4eeaee493b49a1ade7173a17c0a555966892d644
mohamed <moohamedEsam0@gmail.com>
2022-10-13 14:50:37 +0200

    initial with dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,110 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") version "1.7.10-1.0.6"
+}
+
+android {
+    namespace = "com.example.taskmanager"
+    compileSdk = 33
+
+    defaultConfig {
+        applicationId = "com.example.taskmanager"
+        minSdk = 21
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables.useSupportLibrary = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    buildFeatures.compose = true
+    kotlinOptions.jvmTarget = "1.8"
+    composeOptions.kotlinCompilerExtensionVersion = "1.3.1"
+    packagingOptions {
+        resources.excludes += "META-INF/atomicfu.kotlin_module"
+    }
+}
+
+dependencies {
+    val composeVersion: String by rootProject.extra
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.material3:material3:1.0.0-beta03")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
+
+    //icons
+    implementation("androidx.compose.material:material-icons-extended:$composeVersion")
+
+    //navigation
+    val navigationVersion: String by rootProject.extra
+    implementation("androidx.navigation:navigation-compose:$navigationVersion")
+
+    //splash screen
+    implementation("androidx.core:core-splashscreen:1.0.0")
+
+    val koinVersion: String by rootProject.extra
+    val koinComposeVersion: String by rootProject.extra
+    implementation("io.insert-koin:koin-core:$koinVersion")
+    implementation("io.insert-koin:koin-android:$koinVersion")
+    implementation("io.insert-koin:koin-androidx-compose:$koinComposeVersion")
+
+    // Coroutines
+    val coroutinesVersion: String by rootProject.extra
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:$coroutinesVersion")
+
+    // ViewModel
+    val lifecycleVersion: String by rootProject.extra
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    // ViewModel utilities for Compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+
+
+    // coil
+    val coilVersion: String by rootProject.extra
+    implementation("io.coil-kt:coil:$coilVersion")
+    implementation("io.coil-kt:coil-compose:$coilVersion")
+    implementation("io.coil-kt:coil-gif:$coilVersion")
+
+    // room
+    val roomVersion: String by rootProject.extra
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+
+    // To use Kotlin Symbol Processing (KSP)
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$roomVersion")
+
+    //test
+    val truthVersion: String by rootProject.extra
+    testImplementation("com.google.truth:truth:$truthVersion")
+    testImplementation("app.cash.turbine:turbine:0.9.0")
+    testImplementation("org.mockito:mockito-core:2.19.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+val composeVersion by extra { "1.2.1" }
+val koinVersion by extra { "3.2.2" }
+val koinComposeVersion by extra { "3.2.1" }
+val coroutinesVersion by extra { "1.6.4" }
+val coilVersion by extra { "2.2.2" }
+val navigationVersion by extra { "2.5.2" }
+val truthVersion by extra { "1.1.3" }
+val lifecycleVersion by extra { "2.6.0-alpha02" }
+val roomVersion by extra { "2.4.3" }
+
+plugins {
+    id("com.android.application") version "7.3.0" apply false
+    id("com.android.library") version "7.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+
+}
